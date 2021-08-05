c5a2df22b18028da502f78990af784044062866f
bambam <937721540@qq.com>
2021-08-05 16:22:26 +0800

    update

---
+++ Process_data_from_qiita/download_data.sh
@@ -0,0 +1,4 @@
+# Author: Yuzhu Chen
+# You only need to modify the study_id by your own, then this script will help you download raw data from qiita
+# It is recommended to use on the remote server, the download will be faster.
+wget --no-check-certificate 'https://qiita.ucsd.edu/public_download/?data=raw&study_id=10172&study_type=16S'

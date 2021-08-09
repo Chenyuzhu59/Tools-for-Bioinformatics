5c8272fdacce98a41a8c063c6daebc7708183596
bambam <937721540@qq.com>
2021-08-09 15:46:32 +0800

    modify download.sh

--- Process_data_from_qiita/download_data.sh
+++ Process_data_from_qiita/download_data.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # Author: Yuzhu Chen
-# You only need to modify the study_id by your own, then this script will help you download raw data from qiita
+# Usage: sh download_data.sh study_id
+# For example, sh download_data.sh 1038
+# You only need to add the study_id by your own, then this script will help you download raw data from qiita
 # It is recommended to use on the remote server, the download will be faster.
-wget --no-check-certificate 'https://qiita.ucsd.edu/public_download/?data=raw&study_id=10172&study_type=16S'
+
+study_id=$1
+
+wget --no-check-certificate 'https://qiita.ucsd.edu/public_download/?data=raw&study_id='$1'&study_type=16S'
